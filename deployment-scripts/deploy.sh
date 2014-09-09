19e60db4509c20a7dafd3325ea62cc0d69a1614b
Gareth Healy <gahealy@gahealy.csb>
2014-09-09 12:01:23 +0100

    Added local maven repo

--- deployment-scripts/deploy.sh
+++ deployment-scripts/deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# path to fuse install
+FUSE_PATH="/opt/rh/jboss-fuse-6.1.0"
+
 ################################################################################################
 #####             Preconfiguration and helper functions. Skip if not interested.           #####
 ################################################################################################
 
-# set debug mode
-#set -x
+# full path of your ssh, used by the following helper aliases
+SSH_PATH=$(which ssh)
 
 # configure logging to print line numbers
-#export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 # ulimits values needed by the processes inside the container
 ulimit -u 4096
@@ -16,13 +19,6 @@ ulimit -n 4096
 
 # halt on errors
 set -e
-
-FUSE_PATH="/opt/rh/jboss-fuse-6.1.0"
-
-########### aliases to preconfigure ssh and scp verbose to type options
-
-# full path of your ssh, used by the following helper aliases
-SSH_PATH=$(which ssh) 
 
 # alias to connect to the ssh server exposed by JBoss Fuse. uses sshpass to script the password authentication
 alias ssh2fabric="sshpass -p admin $SSH_PATH -p 8101 -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@localhost"
@@ -33,7 +29,9 @@ sh clean_fuse_env.sh
 # start fuse on root node (yes, that initial backslash is required to not use the declared alias)
 "$FUSE_PATH/bin/start"
 
-############################# here you are starting to interact with Fuse/Karaf
+################################################################################################
+#####             			Fuse commands.				           #####
+################################################################################################
 
 # wait for ssh server to be up, avoids "Connection reset by peer" errors
 while ! ssh2fabric "echo up" ; do sleep 1s; done;
@@ -44,9 +42,8 @@ ssh2fabric "wait-for-service -t 300000 io.fabric8.api.BootstrapComplete"
 # create a new fabric AND wait for the Fabric to be up and ready to accept the following commands
 ssh2fabric "fabric:create --clean --resolver localip --global-resolver localip --wait-for-provisioning --profile fabric" 
 
-# configure local nexus
-
-#need to append to or add everything
+# configure local maven/nexus
+ssh2fabric "fabric:profile-edit --append --pid io.fabric8.agent/org.ops4j.pax.url.mvn.repositories=\"/home/gahealy/.m2/repository\" default"
 ssh2fabric "fabric:profile-edit --append --pid io.fabric8.agent/org.ops4j.pax.url.mvn.repositories=\"http://localhost:8081/nexus/content/repositories/releases/@releases@id=local-releases\" default"
 ssh2fabric "fabric:profile-edit --append --pid io.fabric8.agent/org.ops4j.pax.url.mvn.repositories=\"http://localhost:8081/nexus/content/repositories/snapshots/@snapshots@id=local-snapshots\" default"
 
@@ -54,20 +51,17 @@ ssh2fabric "fabric:profile-edit --append --pid io.fabric8.agent/org.ops4j.pax.ur
 ssh2fabric "fabric:profile-edit --pid org.fusesource.fabric.maven/checksumPolicy=warn  default "
 ssh2fabric "fabric:profile-edit --pid org.ops4j.pax.url.mvn/checksumPolicy=warn  default "
 
-
+# install scripts which will create the containers
 #ssh2fabric "shell:source mvn:sample/karaf_scripts/1.0.0-SNAPSHOT/karaf/create_containers"
 #ssh2fabric "shell:source mvn:sample/karaf_scripts/1.0.0-SNAPSHOT/karaf/deploy_codebase"
 
-#set +x
 echo "
 ----------------------------------------------------
 CI Quickstart
 ----------------------------------------------------
 FABRIC ROOT: 
-- ip:          $IP_ROOT
-- karaf:       sshpass -p admin ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null admin@localhost -p8101
+- karaf:       $FUSE_PATH/bin/client -u admin -p admin
 - tail logs:   tail -F $FUSE_PATH/data/log/fuse.log
-
 ----------------------------------------------------
 "
 
