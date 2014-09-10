270e00f6f417a58442bb71a17ff737884d8ac8e6
Gareth Healy <garethahealy@gmail.com>
2014-09-10 17:42:13 +0100

    Tweaks to some of the sh scripts to work with puppet

--- karaf-scripts/deploy-to-mvn.sh
+++ karaf-scripts/deploy-to-mvn.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+###
+# Script to build maven project and deploy
+###
 
 ROOT_POM_PATH=$1
+MVN_PATH=$(which mvn)
 
-mvn clean install deploy -DremoteOBR -f$ROOT_POM_PATH
+$MVN_PATH clean install deploy -DremoteOBR -f$ROOT_POM_PATH
--- run.sh
+++ run.sh
@@ -3,14 +3,17 @@
 # Script to create a Fuse Fabric enviroment, with containers
 ###
 
+# Fuse user
+FUSE_USER=jbossfuse
+LOCAL_USER=gahealy
+
 # Set the below paths accordingly
 ROOT_POM_PATH="/home/gahealy/jboss-studio-workspace/jboss-fuse-setup"
 FUSE_PATH="/opt/rh/jboss-fuse-6.1.0.redhat-379"
 
 SH_PATH=$(which sh)
-SUDO_PATH=$(which sudo)
+ 
+$SH_PATH $ROOT_POM_PATH/karaf-scripts/deploy-to-mvn.sh $ROOT_POM_PATH
 
-$SH_PATH karaf-scripts/deploy-to-mvn.sh $ROOT_POM_PATH
-
-$SUDO_PATH $SH_PATH deployment-scripts/clean_fuse_env.sh $FUSE_PATH
-$SUDO_PATH $SH_PATH deployment-scripts/deploy-to-fuse.sh $FUSE_PATH
+$SH_PATH $ROOT_POM_PATH/deployment-scripts/clean_fuse_env.sh $FUSE_PATH
+$SH_PATH $ROOT_POM_PATH/deployment-scripts/deploy-to-fuse.sh $FUSE_PATH
