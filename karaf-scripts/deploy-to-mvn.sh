a56fdb8575a610c372d093c2f339295ec5ec3aea
Gareth Healy <garethahealy@gmail.com>
2014-11-18 19:43:54 +0000

    Updated poms to match what is done in the examples repo Added source/javadoc call to bash script to make life easier when building from a fresh

--- karaf-scripts/deploy-to-mvn.sh
+++ karaf-scripts/deploy-to-mvn.sh
@@ -7,5 +7,13 @@ ROOT_POM_PATH=$1
 EXAMPLES_POM_PATH=$2
 MVN_PATH=$(which mvn)
 
+# Build the jboss fuse setup and examples projects
 $MVN_PATH clean install deploy -DremoteOBR -f$ROOT_POM_PATH
 $MVN_PATH clean install deploy -DskipTests -DremoteOBR -f$EXAMPLES_POM_PATH
+
+# Download the source and javadocs for both projects
+$MVN_PATH dependency:sources -f$ROOT_POM_PATH
+$MVN_PATH dependency:resolve -Dclassifier=javadoc -f$ROOT_POM_PATH
+
+$MVN_PATH dependency:sources -f$EXAMPLES_POM_PATH
+$MVN_PATH dependency:resolve -Dclassifier=javadoc -f$EXAMPLES_POM_PATH
