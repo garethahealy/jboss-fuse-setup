831d1a0a35377b309d653de53f7ac7529318cd14
Gareth Healy <garethahealy@gmail.com>
2014-11-19 18:48:44 +0000

    Fixed incorrect property name

--- run.sh
+++ run.sh
@@ -8,8 +8,9 @@ FUSE_USER=jbossfuse
 LOCAL_USER=gahealy
 
 # Set the below paths accordingly
-ROOT_POM_PATH="/Users/garethah/Documents/github/garethahealy/jboss-fuse-setup"
-EXAMPLES_POM_PATH="/Users/garethah/Documents/github/garethahealy/jboss-fuse-examples"
+WORKING_DIRECTORY="/Users/garethah/Documents/github/garethahealy"
+ROOT_POM_PATH="$WORKING_DIRECTORY/jboss-fuse-setup"
+EXAMPLES_POM_PATH="$WORKING_DIRECTORY/jboss-fuse-examples"
 FUSE_PATH="/opt/rh/jboss-fuse-6.1.0.redhat-379"
 
 SH_PATH=$(which sh)
