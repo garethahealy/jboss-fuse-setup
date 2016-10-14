f45291253e2fc62c4d1a33c96affa48a742540f4
Gareth Healy <garethahealy@gmail.com>
2016-10-14 14:32:05 +0100

    Added interactive mode to fix weird vagrant issue of getting unknown command; xit Added check for return status to exit if install fails

--- scaffolding-scripts/src/main/resources/install-fuse-and-deploy.sh
+++ scaffolding-scripts/src/main/resources/install-fuse-and-deploy.sh
@@ -9,9 +9,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #      http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,9 +25,10 @@
 ##       && ./install-fuse-and-deploy.sh -e local -u fuse
 
 DEBUG_MODE=false
+INTERACTIVE_MODE=true
 
 ARGS_COUNTER=0
-while getopts ":e:u:x:" opt; do
+while getopts ":e:u:i:x:" opt; do
   ARGS_COUNTER=$[$ARGS_COUNTER +1]
 
   case $opt in
@@ -35,20 +36,23 @@ while getopts ":e:u:x:" opt; do
     ;;
     u) SSH_USER=$OPTARG
     ;;
+    i) INTERACTIVE_MODE=$OPTARG
+    ;;
     x) DEBUG_MODE=$OPTARG
     ;;
     \?)
-    echo -e $RED"Illegal parameters: -$OPTARG"$WHITE
-    echo -e $RED"Usage: ./install-fuse-and-deploy.sh -e (environment) -u (sshuser) -x (debug - optional)"$WHITE
-    echo -e $RED"Example: ./install-fuse-and-deploy.sh -e local -u fuse -x true"$WHITE
+    echo -e $RED"Illegal parameters: -$OPTARG expected: 4"$WHITE
+    echo -e $RED"Usage: ./install-fuse-and-deploy.sh -e (environment) -u (sshuser) -i (interactive mode - default: true) -x (debug - default: false)"$WHITE
+    echo -e $RED"Example: ./install-fuse-and-deploy.sh -e local -u fuse -i true -x true"$WHITE
     exit 1
     ;;
   esac
 done
 
-./install-fuse.sh -e $DEPLOYMENT_ENVIRONMENT -x $DEBUG_MODE
+./install-fuse.sh -e $DEPLOYMENT_ENVIRONMENT -i $INTERACTIVE_MODE -x $DEBUG_MODE
+[ $? -eq 0 ] || exit $?;
 
-cd /opt/rh/scripts/ &&
-    ./deploy.sh -e $DEPLOYMENT_ENVIRONMENT -u $SSH_USER -x $DEBUG_MODE
+./deploy.sh -e $DEPLOYMENT_ENVIRONMENT -u $SSH_USER -i $INTERACTIVE_MODE -x $DEBUG_MODE
+[ $? -eq 0 ] || exit $?;
 
-exit 0;
+exit 0
